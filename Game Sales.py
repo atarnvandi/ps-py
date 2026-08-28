8dc35fe50e108a7a78f543478280ef5cfbb5593e
Ian Nvandi <atarnvandi@gmail.com>
2026-08-28 23:57:29 +0800

    Start import, cleaning, eda, and visualization | not perfect yet, still learing functions

--- Game Sales.py
+++ Game Sales.py
@@ -1,28 +1,22 @@
-#%%
+# %%
 import matplotlib.pyplot as plt
 import pandas as pd
 
-#%% 
+# %% 
 data_dict = pd.read_csv('vg_data_dictionary.csv')
 raw_data = pd.read_csv('vgchartz-2024.csv')
 
-#%%
+# %%
 df = raw_data.copy()
-
-#%%
 # Data Cleaning
 df['release_date'] = pd.to_datetime(df['release_date'], errors='coerce')
 to_fill = {'critic_score': 0,'total_sales': 0,'na_sales': 0,'jp_sales': 0,'pal_sales': 0,'other_sales': 0}
 df = df.fillna(to_fill)
 df['total_sales'] = df[['na_sales', 'pal_sales', 'jp_sales', 'other_sales']].sum(axis=1)
 df = df.drop(columns=['img','last_update'])
+df['developer'] = df['developer'].fillna(df['publisher'])
 
-#%%
-fig, ax = plt.subplots()
-ax.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+# %%
 
-#%%
-fig2, ax2 = plt.subplots()
-ax2.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+# Exploratory Data Analysis
+
