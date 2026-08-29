35aec77d1a306986eb033f50a23168205457c5a6
Ian Nvandi <atarnvandi@gmail.com>
2026-08-29 15:08:18 +0800

    trying create insight as data table

--- Game Sales.py
+++ Game Sales.py
@@ -26,12 +26,33 @@ top5_game = df.groupby(['title'])['total_sales'].sum().to_frame('total_sales').r
 ## 2. Most Selling Game by Genre
 top5_genre = df.groupby(['genre'])['total_sales'].sum().to_frame('total_sales').reset_index().sort_values('total_sales', ascending=False).head(5)
 
+## 3. Most Selling Game by Console
+top5_console = df.groupby(['console'])['total_sales'].sum().to_frame('total_sales').reset_index().sort_values('total_sales', ascending=False).head(5)
+
 # %%
 
 # Visualization
-fig1, ax = plt.subplots(1,2, figsize=(10,5))
+fig1, ax = plt.subplots(1,3, figsize=(10,5))
 ax[0].barh(top5_game['title'], top5_game['total_sales'], height=0.4)
+ax[0].invert_yaxis()
 ax[0].tick_params(axis='x', rotation=90)
 
 ax[1].barh(top5_genre['genre'], top5_genre['total_sales'], height=0.4)
+ax[1].invert_yaxis()
 ax[1].tick_params(axis='x', rotation=90)
+
+ax[2].barh(top5_console['console'], top5_console['total_sales'], height=0.4)
+ax[2].invert_yaxis()
+ax[2].tick_params(axis='x', rotation=90)
+
+# %%
+
+data = {
+        'insight':['Most selling game by title','Most selling game by genre','Most selling game by console'],
+        'finding':[top5_game.iloc[0,0],top5_genre.iloc[0,0],top5_console.iloc[0,0]],
+        'supporting_data':['top5_game','top5_genre','top5_console'],
+        'recomendation':['','','']
+}
+insight_df = pd.DataFrame(data)
+
+insight_df.to_csv('insight.csv',index=False)
