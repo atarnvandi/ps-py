bd2415fe46f01ed6db2a489511649201c01e083a
Ian Nvandi <atarnvandi@gmail.com>
2026-08-31 06:32:11 +0800

    Comparative Analysis: Comparing Sports Vs Action Genre to See How GTA V Become The Most Selling Game

--- Game Sales V2.py
+++ Game Sales V2.py
@@ -20,14 +20,59 @@ df["release_date"] = pd.to_datetime(
 # Exploratory Data Analysis
 
 # 1. Most Selling Game
-top_game_by_sales = df.groupby("title")["total_sales"].sum().nlargest(5)
+top_game_by_sales = (
+    df.groupby("title")["total_sales"]
+    .sum()
+    .to_frame()
+    .nlargest(5, columns="total_sales")
+    .reset_index()
+)
 
-# 2. Variable Comparison - Game by Console
-top_game_by_console = (
+# 2. Most Selling Genre
+top_genre_by_sales = (
+    df.groupby("genre")["total_sales"]
+    .sum()
+    .to_frame()
+    .nlargest(5, columns="total_sales")
+    .reset_index()
+)
+
+# 3. Variable Comparison - Game Sales by Console
+top_game_console_by_sales = (
     df.groupby(["title", "console"])["total_sales"].sum().nlargest(5).reset_index()
 )
 
-# 3. Variable Comparison - Console by Genre
-top_console_by_genre = (
+# 4. Variable Comparison - Genre Sales by Console
+top_console_genre_by_sales = (
     df.groupby(["console", "genre"])["total_sales"].sum().nlargest(5).reset_index()
+)
+
+# There top 5 Genre are Sports, Action, Shooter, Misc, and Racing. As the wide variable after Consoles.
+
+# 5. How's GTA V From Action/ Action-Adventure Bocome The Most Selling Game When The Top Genre is Sports
+## Sports Game Title Sales
+sports_titles_by_sales = (
+    df[df["genre"] == "Sports"]
+    .groupby("title")["total_sales"]
+    .sum()
+    .reset_index()
+    .rename(columns={"total_sales": "sport_sales"})
+)
+## Sports Game Genre On Consoles
+sports_console_by_sales = (
+    df[df["genre"] == "Sports"]
+    .groupby("console")["total_sales"]
+    .sum()
+    .nlargest(5)
+    .reset_index()
+    .rename(columns={"total_sales": "sport_sales"})
+)
+## Sports Game Titles On Console
+sports_titles_by_sales = (
+    df[df["genre"] == "Sports"]
+    .groupby("title")["total_sales"]
+    .sum()
+    .reset_index()
+    .rename(columns={"total_sales": "sport_sales"})
+    .sort_values("sport_sales",ascending=False)
 )
