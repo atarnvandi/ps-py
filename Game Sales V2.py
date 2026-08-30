be72f00790c5086d07c888d63a6d5d6da1cce755
Ian Nvandi <atarnvandi@gmail.com>
2026-08-31 04:47:51 +0800

    EDA and syntax cleaning

--- Game Sales V2.py
+++ Game Sales V2.py
@@ -16,3 +16,18 @@ df[to_fill] = df[to_fill].fillna(0)  # NaN Sales (int) to 0
 df["release_date"] = pd.to_datetime(
     df["release_date"], errors="coerce"
 )  # str to datetime
+# %%
+# Exploratory Data Analysis
+
+# 1. Most Selling Game
+top_game_by_sales = df.groupby("title")["total_sales"].sum().nlargest(5)
+
+# 2. Variable Comparison - Game by Console
+top_game_by_console = (
+    df.groupby(["title", "console"])["total_sales"].sum().nlargest(5).reset_index()
+)
+
+# 3. Variable Comparison - Console by Genre
+top_console_by_genre = (
+    df.groupby(["console", "genre"])["total_sales"].sum().nlargest(5).reset_index()
+)
